b9312fc8a0afaa5517f355a69e07771e7921b63e
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-05-16 20:58:43 +1000

    refact(config): remove process env variables
    Allow the user to provide their config values instead

--- config.js
+++ config.js
@@ -1,22 +1,20 @@
-const DEBUG_MODE_ENV = Boolean(process.env.DEBUG_MODE);
-
 module.exports = {
 
-    ACTIVE_KEY: process.env.ACTIVE_KEY,
-    POSTING_KEY: process.env.POSTING_KEY,
+    ACTIVE_KEY: '',
+    POSTING_KEY: '',
 
-    APP_NAME: process.env.APP_NAME || 'steem-stream',
+    APP_NAME: 'steem-stream',
 
-    USERNAME: process.env.USERNAME ? process.env.USERNAME : '',
+    USERNAME: '',
 
-    LAST_BLOCK_NUMBER: process.env.LAST_BLOCK_NUMBER ? parseInt(process.env.LAST_BLOCK_NUMBER) : 0,
+    LAST_BLOCK_NUMBER: 0,
 
-    BLOCK_CHECK_INTERVAL: process.env.BLOCK_CHECK_INTERVAL ? parseInt(process.env.BLOCK_CHECK_INTERVAL) : 3000,
-    BLOCK_CHECK_WAIT: process.env.BLOCK_CHECK_WAIT ? parseInt(process.env.BLOCK_CHECK_WAIT) : 300,
+    BLOCK_CHECK_INTERVAL: 3000,
+    BLOCK_CHECK_WAIT: 300,
 
-    CHAIN_ID: process.env.CHAIN_ID ? process.env.CHAIN_ID : 'ssc-mainnet1',
-    API_URL: process.env.API_URL ? process.env.API_URL : 'https://api.steemit.com',
+    CHAIN_ID: 'ssc-mainnet1',
+    API_URL: 'https://api.steemit.com',
 
-    DEBUG_MODE: (DEBUG_MODE_ENV === true || DEBUG_MODE_ENV === false) ? DEBUG_MODE_ENV : false
+    DEBUG_MODE: false
 
 };
