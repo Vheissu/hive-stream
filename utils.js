3ea083219e2ea8be07b028ec00330dae46ddd059
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-05-16 22:43:59 +1000

    feat(utils): memory size length method
    Allows us to measure the size of a JSON payload

--- utils.js
+++ utils.js
@@ -1,5 +1,38 @@
 const steem = require('steem');
 
+// https://stackoverflow.com/questions/23318037/size-of-json-object-in-kbs-mbs
+function memorySizeOf(obj) {
+    var bytes = 0;
+
+    function sizeOf(obj) {
+        if(obj !== null && obj !== undefined) {
+            switch(typeof obj) {
+            case 'number':
+                bytes += 8;
+                break;
+            case 'string':
+                bytes += obj.length * 2;
+                break;
+            case 'boolean':
+                bytes += 4;
+                break;
+            case 'object':
+                var objClass = Object.prototype.toString.call(obj).slice(8, -1);
+                if(objClass === 'Object' || objClass === 'Array') {
+                    for(var key in obj) {
+                        if(!obj.hasOwnProperty(key)) continue;
+                        sizeOf(obj[key]);
+                    }
+                } else bytes += obj.toString().length * 2;
+                break;
+            }
+        }
+        return bytes;
+    };
+
+    return sizeOf(obj);
+};
+
 module.exports = {
 
     // https://flaviocopes.com/javascript-sleep/
