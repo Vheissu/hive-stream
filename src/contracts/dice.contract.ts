601de19e4962c26742c87e27787cd8ee9649098f
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-04-02 22:21:46 +1000

    feat(contracts): beginning of example contract

---
+++ src/contracts/dice.contract.ts
@@ -0,0 +1,21 @@
+const CONTRACT_NAME = 'hivedice';
+
+const ACCOUNT = 'hivedice';
+
+const HOUSE_EDGE = 0.05;
+const MIN_BET = 1;
+const MAX_BET = 10;
+
+export default {
+    init: () => {
+        // Runs every time the node starts
+    },
+
+    roll: (payload: { roll: number, amount: string, direction: string }) => {
+        const { roll, amount, direction } = payload;
+        
+        if (roll >= 2 && roll <= 96) {
+
+        }
+    }
+};
