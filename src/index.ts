ebd183d6c674200f8966b7e54bc7e7b6b2d93e24
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-04-06 13:58:38 +1000

    chore(package): expose adapters and types for end user consumption

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export * from './config';
 export * from './streamer';
 export * from './utils';
+export * from './adapters/base.adapter';
+export * from './adapters/file.adapter';
+
+// Types
+export * from './types/hive-stream';
