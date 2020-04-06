66e515124931e8b223e8541df22238373a447445
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-04-06 21:02:51 +1000

    chore(build): release 2.0.01

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ export * from './streamer';
 export * from './utils';
 export * from './adapters/base.adapter';
 export * from './adapters/file.adapter';
+export * from './adapters/sqlite.adapter';
+export * from './adapters/mongodb.adapter';
 
 // Types
 export * from './types/hive-stream';
