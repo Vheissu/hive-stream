f58d07d33edc75e533f13577ea15c71f1b2fdae9
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-04-06 11:49:36 +1000

    feat(adapters): base adapter methods expanded

--- src/adapters/base.adapter.ts
+++ src/adapters/base.adapter.ts
@@ -1,9 +1,30 @@
+import { TransactionType, ContractPayload } from './../types/hive-stream';
 export class AdapterBase {
-    protected loadState() {
+    protected async create() {
+        
+    }
+
+    protected async destroy() {
+        
+    }
+
+    protected async loadState() {
         throw new Error('Load state method not implemented in adapter');
     }
 
     protected async saveState(data: any) {
         throw new Error('Save state method not implemented in adapter');
+    }
+
+    protected async processOperation(op: any, blockNumber: number, blockId: string, prevBlockId: string, trxId: string, blockTime: Date) {
+
+    }
+
+    protected async processTransfer(operation, payload: ContractPayload, metadata: { sender: string, amount: string }) {
+        
+    }
+
+    protected processCustomJson(operation, payload: ContractPayload, metadata: { sender: string, isSignedWithActiveKey: boolean }) {
+
     }
 }
