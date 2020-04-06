8fe1c11f24f5c6ef31e43f264bd93e6c3e243987
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-04-06 14:44:32 +1000

    feat(adapters): sqlite adapter is now feature complete

--- src/adapters/sqlite.adapter.ts
+++ src/adapters/sqlite.adapter.ts
@@ -3,7 +3,7 @@ import { AdapterBase } from './base.adapter';
 
 import { Database } from 'sqlite3';
 
-import path from 'path';
+import path, { resolve } from 'path';
 
 export class SqliteAdapter extends AdapterBase {
     private db = new Database(path.resolve(__dirname, 'hive-stream.db'));
@@ -14,69 +14,92 @@ export class SqliteAdapter extends AdapterBase {
     private prevBlockId;
     private transactionId: string;
 
-    protected async create() {
-        const sql = `
-            CREATE TABLE IF NOT EXISTS params ( id INTEGER PRIMARY KEY, lastBlockNumber NUMERIC )
-            CREATE TABLE IF NOT EXISTS transfers ( id TEXT UNIQUE KEY, blockId TEXT, blockNumber INTEGER, lastBlockNumber INTEGER, sender TEXT, amount TEXT, contractName TEXT, contractAction TEXT, contractPayload TEXT)
-            CREATE TABLE IF NOT EXISTS transactions ( id TEXT UNIQUE KEY, blockId TEXT, blockNumber INTEGER, lastBlockNumber INTEGER, sender TEXT, isSignedWithActiveKey INTEGER, contractName TEXT, contractAction TEXT, contractPayload TEXT)
-        `;
+    protected async create(): Promise<boolean> {
+        return new Promise((resolve) => {
+            this.db.serialize(() => {
+                const params = `CREATE TABLE IF NOT EXISTS params ( id INTEGER PRIMARY KEY, lastBlockNumber NUMERIC )`;
+                const transfers = `CREATE TABLE IF NOT EXISTS transfers ( id TEXT NOT NULL UNIQUE, blockId TEXT, blockNumber INTEGER, sender TEXT, amount TEXT, contractName TEXT, contractAction TEXT, contractPayload TEXT)`;
+                const transactions = `CREATE TABLE IF NOT EXISTS transactions ( id TEXT NOT NULL UNIQUE, blockId TEXT, blockNumber INTEGER, sender TEXT, isSignedWithActiveKey INTEGER, contractName TEXT, contractAction TEXT, contractPayload TEXT)`;
+        
+                this.db.run(params).run(transfers).run(transactions, () => {
+                    resolve(true);
+                });
+            });
+        })
+    }
 
-        this.db.run(sql, [], (err, result) => {
-            return true;
+    protected async loadState(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.db.all('SELECT lastBlockNumber FROM params LIMIT 1', (err, rows) => {
+                if (!err) {
+                    resolve(rows[0]);
+                } else {
+                    reject(err);
+                }
+            });
         });
     }
 
-    protected async loadState() {
-        this.db.all('SELECT lastBlockNumber FROM params LIMIT 1', (err, rows) => {
-            if (!err) {
-                return rows[0];
-            }
-        });
-    }
+    protected async saveState(data: any): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            const sql = `REPLACE INTO params (id, lastBlockNumber) VALUES(1, '${data.lastBlockNumber}')`;
 
-    protected async saveState(data: any) {
-        const sql = `REPLACE INTO params (id, lastBlockNumber) VALUES(1, '${data.lastBlockNumber}')`;
-
-        this.db.run(sql, [], (err, result) => {
-            if (!err) {
-                return true;
-            } else {
-                return err;
-            }
+            this.db.run(sql, [], (err, result) => {
+                if (!err) {
+                    resolve(true);
+                } else {
+                    reject(err);
+                }
+            });
         });
     }
 
     protected async processOperation(op: any, blockNumber: number, blockId: string, prevBlockId: string, trxId: string, blockTime: Date) {
-
+        this.blockNumber = blockNumber;
+        this.blockId = blockId;
+        this.prevBlockId = prevBlockId;
+        this.transactionId = trxId;
     }
 
-    protected async processTransfer(operation, payload: ContractPayload, metadata: { sender: string, amount: string }) {
-        const sql = `INSERT INTO transfers (id, blockId, blockNumber, lastBlockNumber, transactionId, sender, amount, contractName, contractAction, contractPayload) 
-                     VALUES ('${this.transactionId}, '${this.blockId}', ${this.blockNumber}, ${this.lastBlockNumber}, '${metadata.sender}', '${metadata.amount}', '${payload.name}', '${payload.action}', '${JSON.stringify(payload.payload)}')`;
+    protected async processTransfer(operation, payload: ContractPayload, metadata: { sender: string, amount: string }): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            const sql = `INSERT INTO transfers (id, blockId, blockNumber, sender, amount, contractName, contractAction, contractPayload) 
+            VALUES ('${this.transactionId}', '${this.blockId}', ${this.blockNumber}, '${metadata.sender}', '${metadata.amount}', '${payload.name}', '${payload.action}', '${JSON.stringify(payload.payload)}')`;
 
-        this.db.run(sql, [], (err, result) => {
-            if (!err) {
-                return true;
-            } else {
-                return err;
-            }
+            this.db.run(sql, [], (err, result) => {
+                if (!err) {
+                    resolve(true);
+                } else {
+                    reject(err);
+                }
+            });
         });
     }
 
-    protected async processCustomJson(operation, payload: ContractPayload, metadata: { sender: string, isSignedWithActiveKey: boolean }) {
-        const sql = `INSERT INTO transfers (id, blockId, blockNumber, lastBlockNumber, transactionId, sender, isSignedWithActiveKey, contractName, contractAction, contractPayload) 
-                     VALUES ('${this.transactionId}, '${this.blockId}', ${this.blockNumber}, ${this.lastBlockNumber}, '${metadata.sender}', ${metadata.isSignedWithActiveKey}, '${payload.name}', '${payload.action}', '${JSON.stringify(payload.payload)}')`;
+    protected async processCustomJson(operation, payload: ContractPayload, metadata: { sender: string, isSignedWithActiveKey: boolean }): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            const sql = `INSERT INTO transfers (id, blockId, blockNumber, sender, isSignedWithActiveKey, contractName, contractAction, contractPayload) 
+            VALUES ('${this.transactionId}', '${this.blockId}', ${this.blockNumber},'${metadata.sender}', ${metadata.isSignedWithActiveKey}, '${payload.name}', '${payload.action}', '${JSON.stringify(payload.payload)}')`;
 
-        this.db.run(sql, [], (err, result) => {
-            if (!err) {
-                return true;
-            } else {
-                return err;
-            }
+            this.db.run(sql, [], (err, result) => {
+                if (!err) {
+                    resolve(true);
+                } else {
+                    reject(err);
+                }
+            });
         });
     }
 
-    protected async destroy() {
-        this.db.close();
+    protected async destroy(): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            this.db.close((err) => {
+                if (!err) {
+                    resolve(true);
+                } else {
+                    reject(err);
+                }
+            });
+        });
     }
 }
