a4621b1bc5ae719a88f483832661f7ab10a2c8ff
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-04-07 19:40:30 +1000

    feat(adapters): file adapter create json structure in create

--- src/adapters/file.adapter.ts
+++ src/adapters/file.adapter.ts
@@ -1,7 +1,33 @@
 import fs from 'fs';
 import { AdapterBase } from './base.adapter';
+import { TimeAction } from '../actions';
+import { SignedBlock } from '@hivechain/dhive';
 
 export class FileAdapter extends AdapterBase {
+    protected async create(): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            const data = {
+                lastBlockNumber: 0,
+                actions: []
+            };
+    
+            if (!fs.existsSync('hive-stream.json')) {
+                fs.writeFile('hive-stream.json', JSON.stringify(data), err => {
+                    if (err) {
+                        console.error(err);
+        
+                        reject(err);
+                    } else {
+                        resolve(true);
+                    }
+                });
+            } else {
+                resolve(true);
+            }
+        });
+    }
+    }
+    }
     protected loadState() {
         if (fs.existsSync('hive-stream.json')) {
             const state = JSON.parse((fs.readFileSync('hive-stream.json') as unknown) as string);
