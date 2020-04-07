a3ff5e4267808934bbf6e30ec491cfb7fcb6f629
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-04-07 15:05:13 +1000

    feat(contracts): lotto contract

--- src/contracts/lotto.contract.ts
+++ src/contracts/lotto.contract.ts
@@ -1,8 +1,10 @@
+import { sleep } from '@hivechain/dhive/lib/utils';
 import { SqliteAdapter } from './../adapters/sqlite.adapter';
 import { MongodbAdapter } from './../adapters/mongodb.adapter';
 import { Streamer } from '../streamer';
 import seedrandom from 'seedrandom';
 import BigNumber from 'bignumber.js';
+import { Db } from 'mongodb';
 
 const CONTRACT_NAME = 'hivelotto';
 
@@ -12,10 +14,14 @@ const VALID_CURRENCIES = ['HIVE'];
 
 const COST = 10;
 const MAX_ENTRIES = 50;
+const PERCENTAGE_FEE = 25; // 5% of 500 total = 25 HIVE
 
-function rng(previousBlockId, blockId, transactionId) {
+const COLLECTION_LOTTERY = 'lottery';
+const COLLECTION_WINNERS = 'winners';
+
+function rng(previousBlockId, blockId, transactionId, maximum = 100) {
     const random = seedrandom(`${previousBlockId}${blockId}${transactionId}`).double();
-    const randomRoll = Math.floor(random * 100) + 1;
+    const randomRoll = Math.floor(random * maximum) + 1;
 
     return randomRoll;
 }
@@ -84,14 +90,47 @@ class LottoContract {
             }
 
             // Get database reference from adapter
-            const db = this.adapter['db'];
+            const db: Db = this.adapter['db'];
 
-            const collection = db.collection('draws');
+            const collection = db.collection(COLLECTION_LOTTERY);
+            const lotto = await collection.find().sort({'_id': -1}).limit(1).toArray();
+
+            // We have a lotto
+            if (lotto.length) {
+                const total = lotto.entries.length + 1;
+
+                const balance = await this.getBalance();
+                const payout = new BigNumber(balance).minus(PERCENTAGE_FEE).toPrecision(3);
+
+                
+
+                // Total number of entries including this one hits the limit
+                // Lets pay out the lottery
+                if (total === MAX_ENTRIES) {
+                    collection
+
+                    const entrant1 = lotto.entries[rng(this.previousBlockId, this.blockId, this.transactionId, total)];
+
+                    await sleep(3000);
+
+                    const entrant2 = lotto.entries[rng(this.previousBlockId, this.blockId, this.transactionId, total)];
+
+                    await sleep(3000);
+                    
+                    const entrant3 = lotto.entries[rng(this.previousBlockId, this.blockId, this.transactionId, total)];
+
+                    await sleep(3000);
+
+                    const entrant4 = lotto.entries[rng(this.previousBlockId, this.blockId, this.transactionId, total)];
+
+                    await sleep(3000);
+
+                    const entrant5 = lotto.entries[rng(this.previousBlockId, this.blockId, this.transactionId, total)];
+                } else {
+
+                }
+            }
         }
-    }
-
-    draw() {
-
     }
 }
 
