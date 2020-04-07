4fb616420ff7c21eb96154a76b67ffbc5424cbe9
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-04-07 21:35:09 +1000

    feat(actions): add in id and remove cron specific syntax

--- src/actions.ts
+++ src/actions.ts
@@ -1,16 +1,7 @@
-import cron from 'node-cron';
-
-export const TimeValue = {
-    Block: '*/3 * * * * *', // Every 3 seconds
-    Daily: '0 0 */24 * * *', // Every 24 hours
-    Hourly: '0 0 */1 * * *', // Every hour
-    TwelveHours: '0 0 */12 * * *' // Every twelve hours
-};
-
 export class TimeAction {
     date;
 
-    constructor(public timeValue: string, public contractName: string, public contractMethod: string) {
+    constructor(public timeValue: string, public id: string, public contractName: string, public contractMethod: string) {
         this.date = new Date();
     }
 }
