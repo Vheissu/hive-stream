dbf1eae0265d0aae28c97a777e6d4f7534f0abf5
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-04-07 19:41:30 +1000

    chore(package): export actions modelling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * from './config';
 export * from './streamer';
 export * from './utils';
+export * from './actions';
+
 export * from './adapters/base.adapter';
 export * from './adapters/file.adapter';
 export * from './adapters/sqlite.adapter';
