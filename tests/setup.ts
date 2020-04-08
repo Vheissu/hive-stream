222288d4efae54a4e56736794da3b5b5af25321e
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-04-08 20:58:12 +1000

    chore(test): remove console stubbing for now

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,12 +1,12 @@
-(global as any).console = {
-    log: jest.fn(), // console.log are ignored in tests
+// (global as any).console = {
+//     log: jest.fn(), // console.log are ignored in tests
 
-    // Keep native behaviour for other methods, use those to print out things in your own tests, not `console.log`
-    error: console.error,
-    warn: console.warn,
-    info: console.info,
-    debug: console.debug,
-};
+//     // Keep native behaviour for other methods, use those to print out things in your own tests, not `console.log`
+//     error: console.error,
+//     warn: console.warn,
+//     info: console.info,
+//     debug: console.debug,
+// };
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
 
